b47ce0f1cd3d37a5de1377cabc97d64cb05fb8d1
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-02-01 19:37:48 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
-git clone -b KSU-NEXT --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b SUSFS-KSU --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
 
 # Main
-KERNEL_NAME=KSU-NEXT-Kernel # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=KSU-SUSFS-Kernel # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
