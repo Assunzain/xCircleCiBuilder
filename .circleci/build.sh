67d8077db68c42fb79c208bdb7a3e8dfd6594fa6
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-02-03 21:12:36 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Downloading few Dependecies . . ."
+echo "Downloading few Dependecies  . ."
 git clone -b satu --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
 
