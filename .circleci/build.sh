f05476910cf2038728a4f4c459d33637f4868c4b
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-02-28 22:23:08 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies  . ."
 git clone -b 12 --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
-git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
+wget https://github.com/ZyCromerZ/Clang/releases/download/15.0.0-20220726-release/Clang-15.0.0-20220726.tar.gz
+mkdir clang
+tar zxvf Clang-15.0.0-20220726.tar.gz -C clang
 
 # Main
-KERNEL_NAME=SUSFS-Kernel1 # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=SUSFS-Kernel # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
