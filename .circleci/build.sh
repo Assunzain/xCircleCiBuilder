4a9a7dc5fd83a92cae204af68b3dbe759b26df11
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-02-28 12:25:55 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies  . ."
-git clone -b dicoba --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b KSU-SUSFS --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
 
 # Main
-KERNEL_NAME=zain # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=SUSFS-Kernel # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
 CLANG_ROOTDIR=$(pwd)/clang # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=AssunZain # Change with your own name or else.
-export KBUILD_BUILD_HOST=GLKernelBased # Change with your own hostname.
+export KBUILD_BUILD_HOST=ThankYou # Change with your own hostname.
 IMAGE=$(pwd)/X01AD/out/arch/arm64/boot/Image.gz-dtb
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
