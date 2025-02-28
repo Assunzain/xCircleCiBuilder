8db92cb751e5d183d9059cd2607fe50f535d2148
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-02-28 22:36:20 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies  . ."
 git clone -b 12 --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
-wget https://github.com/ZyCromerZ/Clang/releases/download/15.0.0-20220726-release/Clang-15.0.0-20220726.tar.gz
+wget https://github.com/ZyCromerZ/Clang/releases/download/14.0.0-20220202-release/Clang-14.0.0-20220202.tar.gz
 mkdir clang
-tar zxvf Clang-15.0.0-20220726.tar.gz -C clang
+tar zxvf Clang-14.0.0-20220202.tar.gz -C clang
 
 # Main
 KERNEL_NAME=SUSFS-Kernel # IMPORTANT ! Declare your kernel name
