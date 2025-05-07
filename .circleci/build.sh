ca2f0fadebc038f1a66f291b04efe6f73b2dbd06
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-05-07 16:57:54 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies  . ."
-git clone -b KSU-NEXT --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b KSU-SUSFS --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 wget https://github.com/ZyCromerZ/Clang/releases/download/15.0.0-20220217-release/Clang-15.0.0-20220217.tar.gz
 mkdir clang
 tar zxvf Clang-15.0.0-20220217.tar.gz -C clang
 
 # Main
-KERNEL_NAME=KernelSU-v2 # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=KernelSU-SUSFS # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
