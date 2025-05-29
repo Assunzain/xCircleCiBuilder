85bc1c13d9bace20a8e51f8deff10456a0aaa8fe
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2025-05-29 14:31:30 +0700

    Update build-2.sh

--- .circleci/build-2.sh
+++ .circleci/build-2.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "Downloading few Dependecies . . ."
-git clone -b  SUSFS-14 --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
+git clone -b  percobaan --depth=1 https://github.com/Assunzain/Kernel_asus_X01AD X01AD
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r487747c -b 14.0 clang
 
 # Main
-KERNEL_NAME=UPDATE # IMPORTANT ! Declare your kernel name
+KERNEL_NAME=UPDATE1 # IMPORTANT ! Declare your kernel name
 KERNEL_ROOTDIR=$(pwd)/X01AD # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_CODENAME=X01AD # IMPORTANT ! Declare your device codename
 DEVICE_DEFCONFIG=X01AD_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
